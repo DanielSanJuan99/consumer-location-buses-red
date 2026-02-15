ce62ce43007012bdb59ad118553d81581949a91d
Diego Andrés Muñoz Morales <86175879+DimmoX@users.noreply.github.com>
2026-02-15 02:21:05 -0300

    feat: configurar conexión a RabbitMQ y Oracle
    - Agregar application.yml con configuración de datasource
    - Configurar propiedades de RabbitMQ
    - Agregar punto de entrada ConsumerLocationApplication

---
+++ src/main/java/com/busesred/consumer/location/ConsumerLocationApplication.java
@@ -0,0 +1,11 @@
+package com.busesred.consumer.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerLocationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerLocationApplication.class, args);
+    }
+}
